d1c4e67de872a6b8031c11f40afe1d74cfdcee0b
Unknown <Control.yay@gmail.com>
2017-12-30 20:41:37 +0100

    add a winner function
    added a functiont that determines which player won the game

--- app/src/main/java/com/example/user/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/user/tictactoe/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -81,6 +82,79 @@ public class MainActivity extends AppCompatActivity {
             buSelected.setBackgroundColor(Color.BLUE);
             Player2.add(CellID);
             AcitvePlayer=1;
+        }
+        buSelected.setEnabled(false);
+        CheckWinner();
+    }
+
+    void CheckWinner(){
+        int winner=0;
+
+        //the size of the board is pre-determined 3x3
+        //this algorithm can be improved
+        //col1
+        if(Player1.contains(1) && Player1.contains(4) && Player1.contains(7)){
+            winner=1;
+        }else if(Player2.contains(1) && Player2.contains(4) && Player2.contains(7)){
+            winner=2;
+        }
+
+        //col2
+        if(Player1.contains(4) && Player1.contains(5) && Player1.contains(6)){
+            winner=1;
+        }else if(Player2.contains(4) && Player2.contains(5) && Player2.contains(6)){
+            winner=2;
+        }
+
+        //col3
+        if(Player1.contains(7) && Player1.contains(8) && Player1.contains(9)){
+            winner=1;
+        }else if(Player2.contains(7) && Player2.contains(8) && Player2.contains(9)){
+            winner=2;
+        }
+
+        //row1
+        if(Player1.contains(1) && Player1.contains(2) && Player1.contains(3)){
+            winner=1;
+        }else if(Player2.contains(1) && Player2.contains(2) && Player2.contains(3)){
+            winner=2;
+        }
+
+        //row2
+        if(Player1.contains(2) && Player1.contains(5) && Player1.contains(8)){
+            winner=1;
+        }else if(Player2.contains(2) && Player2.contains(5) && Player2.contains(8)){
+            winner=2;
+        }
+
+        //row3
+        if(Player1.contains(3) && Player1.contains(6) && Player1.contains(9)){
+            winner=1;
+        }else if(Player2.contains(3) && Player2.contains(6) && Player2.contains(9)){
+            winner=2;
+        }
+
+        //diag1
+        if(Player1.contains(1) && Player1.contains(5) && Player1.contains(9)){
+            winner=1;
+        }else if(Player2.contains(1) && Player2.contains(5) && Player2.contains(9)){
+            winner=2;
+        }
+
+        //diag2
+        if(Player1.contains(3) && Player1.contains(5) && Player1.contains(7)){
+            winner=1;
+        }else if(Player2.contains(3) && Player2.contains(5) && Player2.contains(7)){
+            winner=2;
+        }
+
+        if(winner!=0){
+            if(winner==1){
+                Toast.makeText(this, "Player 1 won!", Toast.LENGTH_LONG).show();
+            }
+            if(winner==2){
+                Toast.makeText(this, "Player 2 won!", Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
